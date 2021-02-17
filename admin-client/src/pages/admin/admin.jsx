562fbee9e8a3d5087ca8d0d5c5818d54c4f265c7
zzY0123 <932528463@qq.com>
2021-02-17 16:42:54 +0800

    feat: add redurect

--- admin-client/src/pages/admin/admin.jsx
+++ admin-client/src/pages/admin/admin.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
+import {Redirect} from 'react-router-dom'
 
 class Admin extends Component {
 	render () {
-			return <div>Admin</div>
+		// const user = memoryUtils.user
+		// if (!user || !user._id) {
+		// 	return <Redirect to='/login'/>
+		// }
+
+		return (
+			<div>hello</div>
+		)
 	}
 }
 
