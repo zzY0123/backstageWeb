8adaf944957a11216e460fe0856ceeb545553f4f
zzY0123 <932528463@qq.com>
2021-02-19 22:59:59 +0800

    chore: modify leftNav nodeItem

--- admin-client/src/components/leftNav/leftNav.jsx
+++ admin-client/src/components/leftNav/leftNav.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import menuList from '../../config/menuConfig'
+
 import logo from '../../assets/image/leftNav/logo.png'
 
 import './leftNav.css' 
 
 import { Menu } from 'antd'
-import {
-  PieChartOutlined,
-  DesktopOutlined,
-  MailOutlined,
-} from '@ant-design/icons';
 const { SubMenu } = Menu;
 
 class LeftNav extends Component {
@@ -26,54 +23,35 @@ class LeftNav extends Component {
 				<Menu
           			mode="inline"
           			theme="dark"
-       			>	
-       			    <Menu.Item key="/home" icon={<PieChartOutlined />}>
-						<Link to='/home'>
-       						首页	
-						</Link>
-          			</Menu.Item>
-          			<SubMenu key="sub1" icon={<MailOutlined />} title="商品">
-          				<Menu.Item key="/category">
-							<Link to='/category'>
-								品类管理
-							</Link>
-						</Menu.Item>
-            			<Menu.Item key="/product">
-							<Link to='/product'>
-								商品管理
-							</Link>
-						</Menu.Item>
-          			</SubMenu>	
-					<Menu.Item key="/user" icon={<DesktopOutlined />}>
-						<Link to='/user'>
-      	    				用户管理
-						</Link>
-          			</Menu.Item>
-					<Menu.Item key="/role" icon={<DesktopOutlined />}>
-						<Link to='/role'>
-      	    				角色管理
-						</Link>
-          			</Menu.Item>	
-					<SubMenu key="sub2" icon={<MailOutlined />} title="图形图表">
-          				<Menu.Item key="/bar">
-							<Link to='/charts/bar'>
-								柱状图
-							</Link>
-						</Menu.Item>
-            			<Menu.Item key="/line">
-							<Link to='/charts/line'>
-								折线图
-							</Link>
-						</Menu.Item>
-            			<Menu.Item key="/pie">
-							<Link to='/charts/pie'>
-								饼状图
-							</Link>
-						</Menu.Item>
-          			</SubMenu>
-        		</Menu>
+       			>
+					{
+						this.getMenuNodes(menuList)
+					}
+			    </Menu>
 			</div>
 		)
+	}
+
+	getMenuNodes = (menuList) => {
+		return menuList.map(nodeItem => {
+			if (!nodeItem.children) {
+				return (
+					<Menu.Item key={nodeItem.key} icon={nodeItem.icon}>
+						<Link to={nodeItem.key}>
+							{nodeItem.title}
+						</Link>
+					</Menu.Item>
+				)
+			} else {
+				return (
+					<SubMenu key={nodeItem.key} icon={nodeItem.icon} title={nodeItem.title}>
+						{
+							this.getMenuNodes(nodeItem.children)
+						}
+          			</SubMenu>
+				)
+			}
+		})
 	}
 }
 
